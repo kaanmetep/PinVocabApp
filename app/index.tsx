8e7f06fb8d0b388058c3dade4867685795ce2de2
Kaan P. <kaanpmete@gmail.com>
2026-01-08 04:08:23 +0300

    init project

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-sky-500 text-2xl font-bold text-center">
+        PinVocab
+      </Text>
+    </View>
+  );
+}
